20fba2f32538c27b255b8d8582c9ee684fe2f9d3
Md. Shahriar Anwar <anwarshahriar@gmail.com>
2017-01-05 13:53:31 +0000

    Implements time api

--- server.js
+++ server.js
@@ -1,10 +1,41 @@
 var express = require('express')
-var app = express()
+var url = require('url');
+var moment = require('moment');
+var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('*', function (req, res) {
+  var timeData = getProvidedTimeFromURL(req.url);
+  res.json(getTimeResponse(timeData));
+});
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-})
+  console.log('Example app listening on port 8080!');
+});
+
+function getTimeResponse(timeString) {
+  var naturalFormat = 'MMMM DD, YYYY';
+  
+  var respJson = { 
+    "unix": null, 
+    "natural": null 
+  };
+  
+  if (Number(timeString)) {
+    var unixTimeInMillis = Number(timeString);
+    var date = moment.unix(unixTimeInMillis);
+    respJson.unix = unixTimeInMillis;
+    respJson.natural = date.format(naturalFormat);
+  } else {
+    var date = moment(timeString, naturalFormat);
+    if (date.isValid()) {
+      respJson.unix = date.unix();
+      respJson.natural = timeString;
+    }
+  }
+  
+  return respJson;
+}
+
+function getProvidedTimeFromURL(reqURL) {
+  return decodeURI(url.parse(reqURL).pathname.split('/')[1]);
+}
